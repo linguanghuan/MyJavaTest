98237e039397ad62b99de7ddc600624dd3259bc8
linguanghuan <linguanghuan@163.com>
2017-05-22 23:02:14 +0800

    to maven

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# MyJavaTest
-    Java个人测试小程序
-## /MyJavaTest/src/lingh/little/test/ZlibUncompressTest.java
-### linux c zlib解压缩
+
--- src/test/java/com/lingh/compress/ZipTest.java
+++ src/test/java/com/lingh/compress/ZipTest.java
@@ -1,22 +1,25 @@
-package lingh.little.test;
+package com.lingh.compress;
 
+import java.io.UnsupportedEncodingException;
+import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
-import org.apache.commons.codec.binary.Base64;
 
-/**
-* @Author linguanghuan
-* @Time 2017-01-09 14:41:55
-* @Desc 
-*    linux c zlib库 的java解压方式
-*    base64用:commons-codec-1.10.jar http://commons.apache.org/
-*/
-public class ZlibUncompressTest {
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Test;
+
+public class ZipTest {
+
     /**
-     * @Desc
-     *     因为zlib的结果是二进制的形式, 所以这里是用base64编码后的字符串做测试
-     *     base64解码后的二进制就是zlib压缩的格式
+     *  linux c zlib库 的java解压方式
+     *  因为zlib的结果是二进制的形式, 所以这里是用base64编码后的字符串做测试
+     *  base64解码后的二进制就是zlib压缩的格式
+     *  base64用:commons-codec-1.10.jar http://commons.apache.org/
+     *  
+     * @throws DataFormatException 
+     * @throws UnsupportedEncodingException 
      */
-    private static void zlibUncompressTest() throws Exception {
+	@Test
+	public void testDeCompress() throws DataFormatException, UnsupportedEncodingException {
         System.out.println("linux zlib uncompress test");
         String base64Str="eJyFkTsSgzAMRHtOknRIMv5sGSZlaJwmd2DS5f6RgRQMQvHYQ7FPWqGtQH3V5/0BjMAEUCSAB+Dy/szztas7QC8lBgIw9JYmKui3N0QhboS+zN3t4NvroaV6czYQiqu3R0Qg+khSG/EIJp8oOqrKjTK3tNrIblaTKP+IkIHsE9qjLdZBBp0zukSLVM4jl/anLO3RUQ0ttmyFPm1iOVnluBSpyqms9VZcWTukX4cvUYWtUA==";
         byte[] base64DecodedResult = Base64.decodeBase64(base64Str);
@@ -28,9 +31,5 @@ public class ZlibUncompressTest {
         System.out.println(resultLength);
         String decompresserStr = new String(decompressResult, 0, resultLength, "UTF-8");
         System.out.println(decompresserStr);
-    }
-    
-    public static void main(String[] args) throws Exception {
-            zlibUncompressTest();
-    }
+	}
 }
