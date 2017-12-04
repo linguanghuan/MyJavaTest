0fa3008ec3195fd333e9989aba3866c1402dee84
linguanghuan <linguanghuan@gmail.com>
2017-12-04 23:44:15 +0800

    gbk to utf-8

--- src/test/java/com/lingh/collection/HashMapTest.java
+++ src/test/java/com/lingh/collection/HashMapTest.java
@@ -8,14 +8,14 @@ public class HashMapTest {
 	public static void main(String[] args) {
 		// http://yikun.github.io/2015/04/01/Java-HashMap%E5%B7%A5%E4%BD%9C%E5%8E%9F%E7%90%86%E5%8F%8A%E5%AE%9E%E7%8E%B0/
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		map.put("语文", 1);
-		map.put("数学", 2);
-		map.put("英语", 3);
-		map.put("历史", 4);
-		map.put("政治", 5);
-		map.put("地理", 6);
-		map.put("生物", 7);
-		map.put("化学", 8);
+		map.put("璇枃", 1);
+		map.put("鏁板", 2);
+		map.put("鑻辫", 3);
+		map.put("鍘嗗彶", 4);
+		map.put("鏀挎不", 5);
+		map.put("鍦扮悊", 6);
+		map.put("鐢熺墿", 7);
+		map.put("鍖栧", 8);
 		for(Entry<String, Integer> entry : map.entrySet()) {
 		    System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
